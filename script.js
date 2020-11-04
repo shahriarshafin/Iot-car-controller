272b77ddd670448de18c00e2bf9eae9ca9e3cd0b
Shahriar Shafin <32214710+ShahriarShafin@users.noreply.github.com>
2020-11-05 02:49:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,38 +6,54 @@ window.addEventListener('keydown', function (event) {
 	//forward
 	if (event.keyCode === 38) {
 		document.getElementById("forward").src = "icons/up_on.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 	else if (event.keyCode === 87) //W
 	{
 		document.getElementById("forward").src = "icons/up_on.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 
 	//backward
 	if (event.keyCode === 40) {
 		document.getElementById("backward").src = "icons/down_on.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 	else if (event.keyCode === 83) //S
 	{
 		document.getElementById("backward").src = "icons/down_on.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 
 	//left
 	if (event.keyCode === 37) {
 		document.getElementById("left").src = "icons/left_on.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 	else if (event.keyCode === 65) //A
 	{
 		document.getElementById("left").src = "icons/left_on.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 
 	//right
 	if (event.keyCode === 39) {
 		document.getElementById("right").src = "icons/right_on.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 
 	}
 	else if (event.keyCode === 68) //D
 	{
 		document.getElementById("right").src = "icons/right_on.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 
 	}
 
@@ -45,70 +61,102 @@ window.addEventListener('keydown', function (event) {
 	if (event.keyCode === 32) //space
 	{
 		document.getElementById("stop").src = "icons/stop_on.png";
-		notifier();
+		// action link
+		// location.href = "https://shahriarshafin.me";
+		//notifier();
 	}
 
 });
-
+    
 
 //keyup event
 window.addEventListener('keyup', function (event) {
 	//forward 
 	if (event.keyCode === 38) {
 		document.getElementById("forward").src = "icons/up_off.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 	else if (event.keyCode === 87) //W
 	{
 		document.getElementById("forward").src = "icons/up_off.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 
 	//backward
 	if (event.keyCode === 40) {
 		document.getElementById("backward").src = "icons/down_off.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 	else if (event.keyCode === 83) //S
 	{
 		document.getElementById("backward").src = "icons/down_off.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 
 	//left
 	if (event.keyCode === 37) {
 		document.getElementById("left").src = "icons/left_off.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 	else if (event.keyCode === 65) //A
 	{
 		document.getElementById("left").src = "icons/left_off.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 	}
 
 	//right
 	if (event.keyCode === 39) {
 		document.getElementById("right").src = "icons/right_off.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 
 	}
 	else if (event.keyCode === 68) //D
 	{
 		document.getElementById("right").src = "icons/right_off.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 
 	}
 	if (event.keyCode === 32) //space
 	{
 		document.getElementById("stop").src = "icons/stop_off.png";
+		// action link
+		// location.href = "https://shahriarshafin.me";
 
 	}
 
 });
 
-//Notifier
-function notifier() {
+// IP Address 
+function getIp() {
+	var x ="url('";
+	var y = document.getElementById("ip").value;
+	var z ="')"; 
+	var xyz=x+y+z;
+    document.body.style.backgroundImage = xyz;
+    
+  }
+// test code here 
 
-	$('.alert').addClass("show");
-	$('.alert').removeClass("hide");
-	$('.alert').addClass("showAlert");
-	setTimeout(function () {
-		$('.alert').removeClass("show");
-		$('.alert').addClass("hide");
-	}, 1000);
-}
+//demo  http://192.168.0.106:8080/video
 
 // test code here 
 
+//Notifier
+// function notifier() {
+
+// 	$('.alert').addClass("show");
+// 	$('.alert').removeClass("hide");
+// 	$('.alert').addClass("showAlert");
+// 	setTimeout(function () {
+// 		$('.alert').removeClass("show");
+// 		$('.alert').addClass("hide");
+// 	}, 1000);
+// }
