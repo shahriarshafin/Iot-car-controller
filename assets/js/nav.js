23526e7ce92723d347ba45d487153d2859daa926
Shahriar Shafin <shafinshahriar340@gmail.com>
2021-04-12 06:35:16 +0600

    document formated

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,23 +1,23 @@
-$('.expandHome').mouseover(function() {
-  $('.sub-home').css({
+$('.expandHome').mouseover(function () {
+    $('.sub-home').css({
         'display': 'block'
-    }); 
+    });
 });
-$('.subnavbtn').mouseover(function() {
-  $('.sub-home').css({
-        'display': 'none'
-    }); 
-});
-
-$('#trapezoid').mouseleave(function() {
-  $('#trapezoid').css({
-        'margin-top': '-53px'
-    }); 
+$('.subnavbtn').mouseover(function () {
     $('.sub-home').css({
         'display': 'none'
-    }); 
-}).mouseenter(function() {
-  $('#trapezoid').css({
+    });
+});
+
+$('#trapezoid').mouseleave(function () {
+    $('#trapezoid').css({
+        'margin-top': '-53px'
+    });
+    $('.sub-home').css({
+        'display': 'none'
+    });
+}).mouseenter(function () {
+    $('#trapezoid').css({
         'margin-top': '0px'
-    }); 
+    });
 });
