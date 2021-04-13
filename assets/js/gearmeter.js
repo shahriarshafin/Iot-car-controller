81cb6ac64d207f892eb5e37ae5763d04525d41f2
Shahriar Shafin <shafinshahriar340@gmail.com>
2021-04-13 06:27:34 +0600

    ofline audio src added

--- assets/js/gearmeter.js
+++ assets/js/gearmeter.js
@@ -1,158 +1,158 @@
 //--------------------------START SPEED METER------------------------------
 //--------------- https://codepen.io/Chmood/pen/MaBZdM --------------------
 
-// let motorSamplePath = "../audios/gear.ogg";
-let motorSamplePath = "https://freesound.org/data/previews/127/127980_2335231-lq.ogg";
+let motorSamplePath = "../assets/audios/gear.ogg"
+// let motorSamplePath = "https://freesound.org/data/previews/127/127980_2335231-lq.ogg";
 // METER
-let Meter = function Meter($elm, config) {
-  // DOM
-  let $needle, $value;
+class Meter {
+  constructor($elm, config) {
+    // DOM
+    let $needle, $value;
 
-  // Others
-  let steps = (config.valueMax - config.valueMin) / config.valueStep,
-    angleStep = (config.angleMax - config.angleMin) / steps;
+    // Others
+    let steps = (config.valueMax - config.valueMin) / config.valueStep,
+      angleStep = (config.angleMax - config.angleMin) / steps;
 
-  let margin = 10; // in %
-  let angle = 0; // in degrees
+    let margin = 10; // in %
+    let angle = 0; // in degrees
 
-  let value2angle = function (value) {
-    let angle =
-      (value / (config.valueMax - config.valueMin)) *
-      (config.angleMax - config.angleMin) +
-      config.angleMin;
+    let value2angle = function (value) {
+      let angle = (value / (config.valueMax - config.valueMin)) *
+        (config.angleMax - config.angleMin) +
+        config.angleMin;
 
-    return angle;
-  };
+      return angle;
+    };
 
-  this.setValue = function (v) {
-    $needle.style.transform =
-      "translate3d(-50%, 0, 0) rotate(" + Math.round(value2angle(v)) + "deg)";
-    $value.innerHTML = config.needleFormat(v);
-  };
+    this.setValue = function (v) {
+      $needle.style.transform =
+        "translate3d(-50%, 0, 0) rotate(" + Math.round(value2angle(v)) + "deg)";
+      $value.innerHTML = config.needleFormat(v);
+    };
 
-  let switchLabel = function (e) {
-    e.target.closest(".meter").classList.toggle("meter--big-label");
-  };
+    let switchLabel = function (e) {
+      e.target.closest(".meter").classList.toggle("meter--big-label");
+    };
 
-  let makeElement = function (parent, className, innerHtml, style) {
-    let e = document.createElement("div");
-    e.className = className;
+    let makeElement = function (parent, className, innerHtml, style) {
+      let e = document.createElement("div");
+      e.className = className;
 
-    if (innerHtml) {
-      e.innerHTML = innerHtml;
-    }
+      if (innerHtml) {
+        e.innerHTML = innerHtml;
+      }
 
-    if (style) {
-      for (var prop in style) {
-        e.style[prop] = style[prop];
+      if (style) {
+        for (var prop in style) {
+          e.style[prop] = style[prop];
+        }
+      }
+
+      parent.appendChild(e);
+
+      return e;
+    };
+
+    // Label unit
+    makeElement($elm, "label label-unit", config.valueUnit);
+
+    for (let n = 0; n < steps + 1; n++) {
+      let value = config.valueMin + n * config.valueStep;
+      angle = config.angleMin + n * angleStep;
+
+      // Graduation numbers
+      // Red zone
+      let redzoneClass = "";
+      if (value > config.valueRed) {
+        redzoneClass = " redzone";
+      }
+
+      makeElement(
+        $elm,
+        "grad grad--" + n + redzoneClass,
+        config.labelFormat(value), {
+          left: 50 - (50 - margin) * Math.sin(angle * (Math.PI / 180)) + "%",
+          top: 50 + (50 - margin) * Math.cos(angle * (Math.PI / 180)) + "%",
+        }
+      );
+
+      // Tick
+      makeElement($elm, "grad-tick grad-tick--" + n + redzoneClass, "", {
+        left: 50 - 50 * Math.sin(angle * (Math.PI / 180)) + "%",
+        top: 50 + 50 * Math.cos(angle * (Math.PI / 180)) + "%",
+        transform: "translate3d(-50%, 0, 0) rotate(" + (angle + 180) + "deg)",
+      });
+
+      // Half ticks
+      angle += angleStep / 2;
+
+      if (angle < config.angleMax) {
+        makeElement(
+          $elm,
+          "grad-tick grad-tick--half grad-tick--" + n + redzoneClass,
+          "", {
+            left: 50 - 50 * Math.sin(angle * (Math.PI / 180)) + "%",
+            top: 50 + 50 * Math.cos(angle * (Math.PI / 180)) + "%",
+            transform: "translate3d(-50%, 0, 0) rotate(" + (angle + 180) + "deg)",
+          }
+        );
+      }
+
+      // Quarter ticks
+      angle += angleStep / 4;
+
+      if (angle < config.angleMax) {
+        makeElement(
+          $elm,
+          "grad-tick grad-tick--quarter grad-tick--" + n + redzoneClass,
+          "", {
+            left: 50 - 50 * Math.sin(angle * (Math.PI / 180)) + "%",
+            top: 50 + 50 * Math.cos(angle * (Math.PI / 180)) + "%",
+            transform: "translate3d(-50%, 0, 0) rotate(" + (angle + 180) + "deg)",
+          }
+        );
+      }
+
+      angle -= angleStep / 2;
+
+      if (angle < config.angleMax) {
+        makeElement(
+          $elm,
+          "grad-tick grad-tick--quarter grad-tick--" + n + redzoneClass,
+          "", {
+            left: 50 - 50 * Math.sin(angle * (Math.PI / 180)) + "%",
+            top: 50 + 50 * Math.cos(angle * (Math.PI / 180)) + "%",
+            transform: "translate3d(-50%, 0, 0) rotate(" + (angle + 180) + "deg)",
+          }
+        );
       }
     }
 
-    parent.appendChild(e);
+    // NEEDLE
+    angle = value2angle(config.value);
 
-    return e;
-  };
-
-  // Label unit
-  makeElement($elm, "label label-unit", config.valueUnit);
-
-  for (let n = 0; n < steps + 1; n++) {
-    let value = config.valueMin + n * config.valueStep;
-    angle = config.angleMin + n * angleStep;
-
-    // Graduation numbers
-
-    // Red zone
-    let redzoneClass = "";
-    if (value > config.valueRed) {
-      redzoneClass = " redzone";
-    }
-
-    makeElement(
-      $elm,
-      "grad grad--" + n + redzoneClass,
-      config.labelFormat(value), {
-        left: 50 - (50 - margin) * Math.sin(angle * (Math.PI / 180)) + "%",
-        top: 50 + (50 - margin) * Math.cos(angle * (Math.PI / 180)) + "%",
-      }
-    );
-
-    // Tick
-    makeElement($elm, "grad-tick grad-tick--" + n + redzoneClass, "", {
-      left: 50 - 50 * Math.sin(angle * (Math.PI / 180)) + "%",
-      top: 50 + 50 * Math.cos(angle * (Math.PI / 180)) + "%",
-      transform: "translate3d(-50%, 0, 0) rotate(" + (angle + 180) + "deg)",
+    $needle = makeElement($elm, "needle", "", {
+      transform: "translate3d(-50%, 0, 0) rotate(" + angle + "deg)",
     });
 
-    // Half ticks
-    angle += angleStep / 2;
+    let $axle = makeElement($elm, "needle-axle").addEventListener(
+      "click",
+      switchLabel
+    );
+    makeElement(
+      $elm,
+      "label label-value",
+      "<div>" +
+      config.labelFormat(config.value) +
+      "</div>" +
+      "<span>" +
+      config.labelUnit +
+      "</span>"
+    ).addEventListener("click", switchLabel);
 
-    if (angle < config.angleMax) {
-      makeElement(
-        $elm,
-        "grad-tick grad-tick--half grad-tick--" + n + redzoneClass,
-        "", {
-          left: 50 - 50 * Math.sin(angle * (Math.PI / 180)) + "%",
-          top: 50 + 50 * Math.cos(angle * (Math.PI / 180)) + "%",
-          transform: "translate3d(-50%, 0, 0) rotate(" + (angle + 180) + "deg)",
-        }
-      );
-    }
-
-    // Quarter ticks
-    angle += angleStep / 4;
-
-    if (angle < config.angleMax) {
-      makeElement(
-        $elm,
-        "grad-tick grad-tick--quarter grad-tick--" + n + redzoneClass,
-        "", {
-          left: 50 - 50 * Math.sin(angle * (Math.PI / 180)) + "%",
-          top: 50 + 50 * Math.cos(angle * (Math.PI / 180)) + "%",
-          transform: "translate3d(-50%, 0, 0) rotate(" + (angle + 180) + "deg)",
-        }
-      );
-    }
-
-    angle -= angleStep / 2;
-
-    if (angle < config.angleMax) {
-      makeElement(
-        $elm,
-        "grad-tick grad-tick--quarter grad-tick--" + n + redzoneClass,
-        "", {
-          left: 50 - 50 * Math.sin(angle * (Math.PI / 180)) + "%",
-          top: 50 + 50 * Math.cos(angle * (Math.PI / 180)) + "%",
-          transform: "translate3d(-50%, 0, 0) rotate(" + (angle + 180) + "deg)",
-        }
-      );
-    }
+    $value = $elm.querySelector(".label-value div");
   }
-
-  // NEEDLE
-  angle = value2angle(config.value);
-
-  $needle = makeElement($elm, "needle", "", {
-    transform: "translate3d(-50%, 0, 0) rotate(" + angle + "deg)",
-  });
-
-  let $axle = makeElement($elm, "needle-axle").addEventListener(
-    "click",
-    switchLabel
-  );
-  makeElement(
-    $elm,
-    "label label-value",
-    "<div>" +
-    config.labelFormat(config.value) +
-    "</div>" +
-    "<span>" +
-    config.labelUnit +
-    "</span>"
-  ).addEventListener("click", switchLabel);
-
-  $value = $elm.querySelector(".label-value div");
-};
+}
 
 // DOM LOADED FIESTA
 document.addEventListener("DOMContentLoaded", function () {
